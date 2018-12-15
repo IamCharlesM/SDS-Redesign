2b82dc8e2fd0bf065b7e170b385a291dd31b3ba5
iamcharlesm <because@charlesis.me>
2018-12-15 11:58:32 -0600

    Moved nav component into main component, updated home page.

--- sds/src/App.vue
+++ sds/src/App.vue
@@ -1,29 +1,94 @@
 <template>
-<body>
+
  <div id="app">
-    <Navbar />
+   <link href="https://cdn.jsdelivr.net/npm/animate.css@3.5.1" rel="stylesheet" type="text/css">
+<!-- <section class="hero is-dark is-medium"> -->
+  <!-- Hero head: will stick at the top -->
+  <!-- <div class="hero-head"> -->
+    <nav class="navbar is-black">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="./assets/placeholder.jpg" alt="Logo">
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroA" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">
+              Home
+            </a>
+           
+      <a class="navbar-item">
+        Registration
+      </a>
+
+      <a class="navbar-item">
+        Schedule
+      </a>
+            <div class="navbar-item has-dropdown is-hoverable is-boxed ">
+        <a class="navbar-link ">
+          More
+        </a>
+
+        <div class="navbar-dropdown is-boxed ">
+          <a class="navbar-item">
+            Legacy
+          </a>
+          <a class="navbar-item">
+            Staff
+          </a>
+          <a class="navbar-item">
+            Contact Us
+          </a>
+        
+        </div>
+      </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  <!-- </div> -->
+  <!-- </section> -->
+
     <Home />
   </div>
-</body>
 </template>
 
 <script>
-  import Navbar from './components/Navbar.vue'
+  // import Navbar from './components/Navbar.vue'
   import Home from './components/Home.vue'
   // import Registration from './components/Registration.vue'
 export default {
   name: 'app',
   components: {
-    Navbar,
+    // Navbar,
     Home,
     // Registration
   }
 }
 </script>
 
-<style>
-#app {
-  /* background-color: black; */
-  background-image: url("./assets/wall.jpg")
+<style scoped>
+/* #app {
+  background-color: black;
+  background-image: url("./assets/wall.jpg") !important;
+  background-size: cover;
+} */
+
+.navbar-dropdown {
+  /* background-color: hsl(0, 0%, 21%) */
+  background-color: black
+}
+.navbar-dropdown a:hover {
+  color: black !important
+}
+
+.navbar-dropdown a {
+  color: white !important
 }
 </style>
--- sds/src/components/Home.vue
+++ sds/src/components/Home.vue
@@ -1,10 +1,13 @@
 <template>
 <section class="hero is-dark has-text-centered is-fullheight-with-navbar">
   <div class="hero-body">
-    <div class="container">
-      <p class="title">
-        The purpose of the Sammy Dyer School of the Theatre is to promote the progress of creativity and education in the performing arts as well as to assist children in receiving quality instruction in technique and dance appreciation. In addition, we hope to bring together professionals and students interested in performing arts education by providing resources and the opportunity to realize their visions. We hope to preserve and develop the legacy of the African American dance traditions of the mid to late 20th century.
+    <div class="container animated fadeIn">
+      <p class="title slow">
+        85 years of dedication to the Performing arts.
       </p>
+      <hr>
+      <p>The purpose of the Sammy Dyer School of the Theatre is to promote the progress of creativity and education in the performing arts as well as to assist children in receiving quality instruction in technique and dance appreciation.
+In addition, we hope to bring together professionals and students interested in performing arts education by providing resources and the opportunity to realize their visions. We hope to preserve and develop the legacy of the African American dance traditions of the mid to late 20th century.</p>
     </div>
   </div>
 </section>
@@ -16,5 +19,19 @@ export default {
   
 }
 </script>
-<style>
+<style scoped>
+.hero-body{
+  /* Both are required for the image to appear */
+background-image: url("../assets/wall.jpg") !important ;
+        background-position: center center;
+        background-repeat:  no-repeat;
+        background-attachment: fixed;
+        background-size:  cover;
+        background-color: black;
+}
+
+.fadeIn {
+  animation-duration: 2s;
+}
+
 </style>
