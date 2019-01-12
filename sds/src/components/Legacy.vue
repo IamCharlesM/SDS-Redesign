294c752a39e07ecf153ec6db6b9ab08f5b57746a
iamcharlesm <because@charlesis.me>
2019-01-12 00:48:06 -0600

    Legacy page updated.

--- sds/src/components/Legacy.vue
+++ sds/src/components/Legacy.vue
@@ -1,40 +1,105 @@
 <template>
-<section class="hero has-text-centered is-white ">
+  <section class="hero has-text-centered is-white ">
     <div class="hero-body has-text-white">
       <div class="container animated fadeIn">
-      <div class="columns">
-        <div class="column bg">
-      <p class="title slow">
-        This is the Legacy page
-      </p>
-      <hr>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda laboriosam rerum dicta repudiandae molestias dignissimos ipsam, a facilis atque aliquid ex veniam sit, corrupti dolorum odio ab iste ipsa nostrum.</p>
-      </div>
+        <div class="columns">
+          <div class="column bg">
+            <p class="title slow">
+              The Legacy
+            </p>
+            <hr>
+            <div class="columns">
+              <div class="column">
+                <div class="image is-128x128 is-centered">
+                  <img class="is-rounded" src="../assets/sammydyer.jpg">
+                </div>
+                <br>
+                <p class="title is-size-5">Sammy Vashon Dyer (1933-1960) - Founder/Director</p>
+                <p class="subtitle">The Sammy Dyer School of Dancing was established in May 1933 by noted Choreographer
+                  Sammy Dyer.
 
+                  Dyer, an Instructor of Tap Dance and stage shows for Muriel Abbott and the famous Abbotteers,
+                  produced cafe shows at the old Sunset Cafe, Club Morocco, and the Club DeLisa where he spent eighteen
+                  years. In 1950 Sammy Dyer created the Dyerettes, an African American female dance troupe known to be
+                  the only chorus act of its kind. The Dyerettes toured internationally with some of the greatest
+                  entertainers of the twentieth century including Nat King Cole, Sammy Davis Jr., and Sarah Vaughn. Mr.
+                  Dyer's desire to provide affordable dance education to African American children resulted in the
+                  establishment of The Sammy Dyer School of the Theatre. Today the legacy continues as the School of
+                  Theatre is entering its 85th year.</p>
+
+              </div>
+              <div class="column">
+
+                <div class="image is-128x128 is-centered">
+                  <img class="is-rounded" src="../assets/Shirleyhall.jpg">
+                </div>
+                <br>
+                <p class="title is-size-5">
+                  Shirley Hall Bass
+                  (1960-1998)
+                  Artistic Director</p>
+                <p class="subtitle"> In 1980, at Mr. Dyer's request, the Directorship of the school was passed to
+                  Dyerette Shirley Hall Bass. In 1968, Mrs. Bass traveled to the Bahamas Islands with 30 Sammy Dyer
+                  students, beginning the Cultural Exchange Workshop. In this program students from the Bahamas Dance
+                  Theatre and Sammy Dyer School travel each year to share culture and tradition through dance and
+                  friendship. In recent years the Chicago school has hosted an array of exciting dance workshops and
+                  seminars including "Tap Explosion", which featured Broadway star and instructorr, Savion Glover.
+                  Students of Sammy Dyer have also placed 1st in numerous competitions on regional and national levels.</p>
+
+              </div>
+
+              <div class="column">
+
+                <div class="image is-128x128 is-centered">
+                  <img class="is-rounded" src="../assets/levy.jpg">
+                </div>
+                <br>
+                <p class="title is-size-5">Ted Levy (1999-2001) Artistic Consultant</p>
+                <p class="subtitle">
+                  Ted Levy began his training at The Sammy Dyer School of the Theatre, under the tutelage of Mrs.
+                  Shirley Hall-Bass and Mrs. Muriel Burns Wilson Foster. He intensified his training at Columbia
+                  College of Chicago, and provided opportunity for his professional careerr, which began at the Victory
+                  Gardens Theater. Ted won an Emmy Award for his television debut performance in the PBS Special
+                  "Precious Memories", abd impressed the professional dance world in 1990 with his Broadway debut in
+                  "Black and Blue". He has since collaborated with George C. Wolfe and Gregory Hines on "Jelly's Last
+                  Jam," which won an Outer Critics Circle Award in 1993, and the Antoinette Perry Drama Desk nomination
+                  for choreography. Levy hosted "Ted Levy and Friends," directed by Gregory Hines and was assistant
+                  choreographer to Savion Glover for Broadway's 1996 Tony Award Winner, "Bring in 'da Noise, Bring in
+                  'da Funk." He has perfomed in China with the Chicago Human Rhythm Project as well as taught workshops
+                  and performed with Annual Rhythm World.
+
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
       </div>
-    </div>
     </div>
   </section>
 </template>
 
 <script>
+  export default {
+    name: 'Legacy',
 
-export default {
-  name: 'Legacy',
+  }
 
-}
 </script>
 <style scoped>
-.hero-body {
-  /* Both are required for the image to appear */
-        /* background-image: url("../assets/ALL NEW-2108.jpg") !important ; */
-        /* background-position: center center; */
-        /* background-repeat:  no-repeat; */
-        /* background-attachment: fixed; */
-        /* background-size:  cover; */
-        /* color: black */
-}
+  .image {
+    margin: 0 auto;
+  }
 
-
+  .hero-body {
+    /* Both are required for the image to appear */
+    /* background-image: url("../assets/ALL NEW-2108.jpg") !important ; */
+    /* background-position: center center; */
+    /* background-repeat:  no-repeat; */
+    /* background-attachment: fixed; */
+    /* background-size:  cover; */
+    /* color: black */
+  }
 
 </style>
